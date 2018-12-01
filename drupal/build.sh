c6ef0fe4b2eb3421a452452ee99b1a6e79825e43
John <lijd@eabax.com>
2018-12-01 09:31:55 +0800

    fix.

---
+++ drupal/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+docker pull cobers/drupal
+
+docker rm -f drupal
+docker run -d --name drupal cobers/drupal
+if [ -d ./web.old ]; then
+  sudo rm -rf ./web.old
+fi
+if [ -d ./web ]; then
+  mv ./web ./web.old
+fi
+sudo docker cp drupal:/var/www/html ./web
+docker stop drupal
+docker rm drupal
+
+sudo chown -R $USER ./web
+sudo chmod -R g+w ./web
